10adfaa2317ce248dc913fd9f056ce3fbd6252f9
iriri <iri@konnichiwastevenspielbergde.su>
2018-08-13 00:16:27 -0700

    It works

--- lwt/lwt-x64.s
+++ lwt/lwt-x64.s
@@ -4,19 +4,22 @@ lwt$swap:
 _lwt$swap:
 	movq	%rsp, (%rdi)
 	movq	(%rsi), %rsp
-	popq	%rax
 
-	movq	%rbp, 0x08(%rdi)
+	movq	%rax, 0x08(%rdi)
 	movq	%rbx, 0x10(%rdi)
-	movq	%r12, 0x18(%rdi)
-	movq	%r13, 0x20(%rdi)
-	movq	%r14, 0x28(%rdi)
-	movq	%r15, 0x30(%rdi)
+	movq	%rbp, 0x18(%rdi)
+	movq	%r12, 0x20(%rdi)
+	movq	%r13, 0x28(%rdi)
+	movq	%r14, 0x30(%rdi)
+	movq	%r15, 0x38(%rdi)
 
-	movq	0x08(%rsi), %rbp
+	movq	0x08(%rsi), %rax
 	movq	0x10(%rsi), %rbx
-	movq	0x18(%rsi), %r12
-	movq	0x20(%rsi), %r13
-	movq	0x28(%rsi), %r14
-	movq	0x30(%rsi), %r15
-	jmpq	*%rax
+	movq	0x18(%rsi), %rbp
+	movq	0x20(%rsi), %r12
+	movq	0x28(%rsi), %r13
+	movq	0x30(%rsi), %r14
+	movq	0x38(%rsi), %r15
+
+	addq	$0x8, %rsp
+	jmpq	*-0x8(%rsp)
