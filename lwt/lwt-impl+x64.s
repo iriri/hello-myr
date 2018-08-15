4cde49f55244e2b68a0029d81e243c5d2b1e7844
iriri <iri@konnichiwastevenspielbergde.su>
2018-08-15 11:44:30 -0700

    it can't be done it's impossible

--- lwt/lwt-impl+x64.s
+++ lwt/lwt-impl+x64.s
@@ -1,25 +1,22 @@
 .globl lwt$swap
-.global _lwt$swap
+.globl _lwt$swap
 lwt$swap:
 _lwt$swap:
-	movq	%rsp, (%rdi)
-	movq	(%rsi), %rsp
-
-	movq	%rax, 0x08(%rdi)
-	movq	%rbx, 0x10(%rdi)
-	movq	%rbp, 0x18(%rdi)
+	movq	%rsp, 0x00(%rdi)
+	movq	%rbp, 0x08(%rdi)
+	movq	%rax, 0x10(%rdi)
+	movq	%rbx, 0x18(%rdi)
 	movq	%r12, 0x20(%rdi)
 	movq	%r13, 0x28(%rdi)
 	movq	%r14, 0x30(%rdi)
 	movq	%r15, 0x38(%rdi)
 
-	movq	0x08(%rsi), %rax
-	movq	0x10(%rsi), %rbx
-	movq	0x18(%rsi), %rbp
-	movq	0x20(%rsi), %r12
-	movq	0x28(%rsi), %r13
-	movq	0x30(%rsi), %r14
 	movq	0x38(%rsi), %r15
-
-	addq	$0x8, %rsp
-	jmpq	*-0x8(%rsp)
+	movq	0x30(%rsi), %r14
+	movq	0x28(%rsi), %r13
+	movq	0x20(%rsi), %r12
+	movq	0x18(%rsi), %rbx
+	movq	0x10(%rsi), %rax
+	movq	0x08(%rsi), %rbp
+	movq	0x00(%rsi), %rsp
+	retq
